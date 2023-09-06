0b5760ad35f05bd6d0785b16c68d328bd4620cbf
Yingshan <ybi11@jh.edu>
2023-09-06 17:26:28 -0400

    actually update the script for maleplot

--- day2-homework/plot-sisA.py
+++ day2-homework/plot-sisA.py
@@ -17,27 +17,37 @@ for i in range(len(transcripts)):
     if transcripts[i] == 'FBtr0073461':
         row = i
 
-# Find columns with samples of interest
+# Find columns with samples of interest in female
 cols = []
 for i in range(len(samples)):
     if "female" in samples[i]:
         cols.append(i)
 
-# Subset data of interest
+cols_male = []
+for i in range(len(samples)):
+	if "female" not in samples[i]:
+		cols_male.append(i)
+
+#Subset data of interest
 expression = data[row, cols]
+expression_male = data[row,cols_male]
 
 # Prepare data
-x = samples[cols]
+x = ["10", "11", "12", "13", "14A", "14B", "14C", "14D"]
 y = expression
+y1 = expression_male
+
 
 # Plot data
 fig, ax = plt.subplots()
 plt.xticks(rotation = 45)
 ax.set_title( "Sxl (FBtr0073461)" )
-ax.plot( x, y )
+ax.plot( x, y)
+ax.plot( x, y1)
 ax.set_xlabel("Developmental Stage")
 ax.set_ylabel(" mRNA abundance(RPKM)")
+ax.legend(['female','male'])
 plt.tight_layout() #order matters, has to be after this 
-fig.savefig( "FBtr0073461.png" )
+fig.savefig( "FBtr0073461_maleplot.png" )
 plt.show()
-#plt.close( fig )
+# #plt.close( fig )
