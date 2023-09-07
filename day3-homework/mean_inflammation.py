afa635ab1dbc6d4be42baac716eaab63598ce4d3
Yingshan <ybi11@jh.edu>
2023-09-07 18:12:46 -0400

    updated mean_inflmmation python script

--- day3-homework/mean_inflammation.py
+++ day3-homework/mean_inflammation.py
@@ -12,15 +12,18 @@ for line in lines:
 	newlist.append(line_list)# Now you added splited them and construct a new list using append (create one first)
 
  #need to be assigned first
-def mean_in(newlist):
+def mean_in(patient, newlist):
 	average_list=[]
 	total = 0
+	patient = 0
+	patient in range(len(newlist))
+	print(type(patient))
 	for eachpatient in newlist: 
 		for string in eachpatient:
 			integer = int(string)
 			total += integer
 		average = total/len(eachpatient)
 		average_list.append(average) # so far here you are looping within the patient level 
-	return average_list #indasation matters, you need to go back to calculate for every patient
+	return average_list[patient] #indasation matters, you need to go back to calculate for every patient
 
-print(mean_in(newlist))
+print(mean_in(1, newlist))
