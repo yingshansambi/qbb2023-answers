0d27f180163dcdc78a0ffc7ab2da10ad8152dc6a
Yingshan <ybi11@jh.edu>
2023-09-07 17:26:18 -0400

    mean_inflammation python script

---
+++ day3-homework/mean_inflammation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+
+f = open('inflammation-01.csv', 'r')
+
+lines = f.readlines()
+
+newlist=[]
+
+for line in lines:
+	line = line.rstrip() # It's still integer
+	line_list = line.split(',') # Now becomes a list for each patient
+	newlist.append(line_list)# Now you added splited them and construct a new list using append (create one first)
+
+ #need to be assigned first
+def mean_in(newlist):
+	average_list=[]
+	total = 0
+	for eachpatient in newlist: 
+		for string in eachpatient:
+			integer = int(string)
+			total += integer
+		average = total/len(eachpatient)
+		average_list.append(average) # so far here you are looping within the patient level 
+	return average_list #indasation matters, you need to go back to calculate for every patient
+
+print(mean_in(newlist))
