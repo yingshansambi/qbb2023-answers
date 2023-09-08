0067356ee3a32a93c0398c13a212ebf5282f876e
Yingshan <ybi11@jh.edu>
2023-09-08 14:18:56 -0400

    Exercise2-P1-Plot of multiple allele trajectories

--- day4/inclass.py
+++ day4/inclass.py
@@ -17,6 +17,34 @@
 # print(WFModel(0.5,200))
 
 
+# import numpy as np 
+# import matplotlib.pyplot as plt 
+
+# def WFModel(AF,Pop): #AF = allele frequency 
+# 	AFlist = []
+# 	i = 0
+# 	gen = []
+# 	while 0< AF < 1: 
+# 		i = i + 1 
+# 		num = np.random.binomial(2*Pop, AF)
+# 		AF = num/(2*Pop)
+# 		AFlist.append(AF)
+# 		gen.append(i)
+# 	return [AFlist, gen]
+
+# trajectory = WFModel(0.5,200)
+# fig, ax = plt.subplots()
+# x_positions = trajectory[1]
+# y_positions = trajectory[0]
+# ax.set_title( "The Wright-Fisher Model" )
+# ax.set_xlabel("Generations")
+# ax.set_ylabel("Allele Frequency")
+# ax.plot(x_positions, y_positions)
+# fig.savefig( "WF.png" )
+# plt.show()
+
+#Question 2 
+
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -32,15 +60,16 @@ def WFModel(AF,Pop): #AF = allele frequency
 		gen.append(i)
 	return [AFlist, gen]
 
-trajectory = WFModel(0.5,200)
+
+
 fig, ax = plt.subplots()
-x_positions = trajectory[1]
-y_positions = trajectory[0]
-ax.set_title( "The Wright-Fisher Model" )
-ax.set_xlabel("Generations")
-ax.set_ylabel("Allele Frequency")
-ax.plot(x_positions, y_positions)
-fig.savefig( "WF.png" )
+for i in range(100):
+	trajectory = WFModel(0.5,200)
+	x_positions = trajectory[1]
+	y_positions = trajectory[0]
+	ax.set_title( "The Wright-Fisher Model(Repeats = 100)" )
+	ax.set_xlabel("Generations")
+	ax.set_ylabel("Allele Frequency")
+	ax.plot(x_positions,y_positions)
+fig.savefig( "WF-Part2-Plot1.png" )
 plt.show()
-
-
