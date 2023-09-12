77ab18d4515d42958450998ac4672104d0326e13
Yingshan <ybi11@jh.edu>
2023-09-11 21:52:38 -0400

    Basic Questions

--- day4/inclass.py
+++ day4/inclass.py
@@ -145,28 +145,48 @@ def WFModel(AF,Pop): #AF = allele frequency
 
 # Exerise 3 - Part 2 
 
-frequency = [0.01, 0.1, 0.25, 0.5, 0.75]
-genlist_2 = []
-for freq in frequency:
- 	for i in range(10):
- 		trajectory = WFModel(freq,1000) # with fixed population = 1000
- 		genlist_2.append(trajectory[1])
-#print(genlist_2)
+# frequency = [0.01, 0.1, 0.25, 0.5, 0.75]
+# genlist_2 = []
+# for freq in frequency:
+#  	for i in range(10):
+#  		trajectory = WFModel(freq,1000) # with fixed population = 1000
+#  		genlist_2.append(trajectory[1])
+# #print(genlist_2)
 
-averagelist_2=[]
-for i in (0, 1, 2, 3, 4):
-	startPos = i
-	stopPos = i + 9 
-	averagelist_2.append(sum(genlist_2[startPos:stopPos])/10)
-#print(averagelist_2)
+# averagelist_2=[]
+# for i in (0, 1, 2, 3, 4):
+# 	startPos = i
+# 	stopPos = i + 9 
+# 	averagelist_2.append(sum(genlist_2[startPos:stopPos])/10)
+# #print(averagelist_2)
 
-fig, ax = plt.subplots()
-ax.scatter(frequency, averagelist_2)
-ax.set_xlabel("Allele Frequency")
-ax.set_ylabel("Avaerge Time to Fixation(year)")
-ax.set_title( "Average Time to Fixation across Allele Frequencies" )
-fig.savefig( "Average_Time_vs_Allele_Frequencies" )
-plt.show()
+# fig, ax = plt.subplots()
+# ax.scatter(frequency, averagelist_2)
+# ax.set_xlabel("Allele Frequency")
+# ax.set_ylabel("Avaerge Time to Fixation(year)")
+# ax.set_title( "Average Time to Fixation across Allele Frequencies" )
+# fig.savefig( "Average_Time_vs_Allele_Frequencies" )
+# plt.show()
+
+
+#Basic Exerise 
+
+#1. 
+#For the first plot of Question 1, the allele frequency is dropping toward to zero with fluctations.
+#We can see the this gene will eventually dispear in the population. 
+#What might be contributing could be environmental changes that did not favor the allele. 
+#Biologically, it could be the phenotype does not offer any selection advantges for this
+#population to survive. 
+
+#2 
+#One of the assumptions such as "everyone reproduces once per generation"
+#In reality, there might be still a portion of the population can't be able to reproduce due to 
+#many reseasons, such as natural disasters, diseases. Therefore, the genetic information from allele might not be able to 
+#be passed to the next generation. 
+
+#3
+#Another assumpiton like "no mutation", which could be violated by potential errors generated in every possess during 
+#protein expression, from transcription to post-translation. Mutations will offer more genetic diversity to the allele. 
 
 
 
