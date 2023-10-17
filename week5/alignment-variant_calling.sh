cc00d569f5ea24ec97a2801fbef0dce6ae2ffe1b
Yingshan <ybi11@jh.edu>
2023-10-17 17:58:12 -0400

    step1.3

--- week5/alignment-variant_calling.sh
+++ week5/alignment-variant_calling.sh
@@ -6,11 +6,26 @@
 
 #{sample}already includes the filetype
 #Step1.2 
-for sample in *.fastq
-do
-	echo "Aligning sample:" ${sample}
-	bwa mem -t 4 -R "@RG\tID:${sample}\tSM:${sample}" \ 
-	  sacCer3.fa \
-	  ${sample} > ${sample}.sam
-done
+# for sample in *.fastq
+# do
+# 	echo "Aligning sample:" ${sample}
+# 	bwa mem -t 4 -R "@RG\tID:${sample}\tSM:${sample}" \ 
+# 	  sacCer3.fa \
+# 	  ${sample} > ${sample}.sam
+# done
 
+# Step1.3
+# for sample in A01_09 A01_11 A01_23 A01_24 A01_27 A01_31 A01_35 A01_39 A01_62 A01_63
+# # for sort in *.sam
+# do
+# 	inFile=${sample}.fastq.sam
+# 	outFile=${sample}.sorted.bam
+# 	samtools sort -o ${outFile} -O BAM ${inFile} #${}label the variable.
+# done
+
+for sample_index in A01_09 A01_11 A01_23 A01_24 A01_27 A01_31 A01_35 A01_39 A01_62 A01_63
+do 
+	inPut=${sample_index}.sorted.bam
+	outPut=${sample_index}.sorted.bam.bai
+	samtools index -M -b ${inPut} 
+done
